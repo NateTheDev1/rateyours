bffaf04a4976a8ef79ae7e13751d05fe10513df1
Nathaniel Richards <nrichardsbusiness@gmail.com>
2021-08-19 23:36:02 -0400

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { hydrate, render } from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './normalize.scss';
+import './tailwind.css';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootElement = document.getElementById('root')!;
+
+if (rootElement.hasChildNodes()) {
+	hydrate(<h1>Hello</h1>, rootElement);
+} else {
+	render(<h1>Hello</h1>, rootElement);
+}
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {},
+    borderColor: (theme) => ({
+      ...theme("colors"),
+      DEFAULT: theme("colors.gray.300", "currentColor"),
+      dndRed: "#E6010A",
+    }),
+    backgroundColor: (theme) => ({
+      ...theme("colors"),
+      bgmain: "#101519",
+    }),
+  },
+  variants: {
+    backgroundColor: ["responsive", "hover", "focus", "group-hover"],
+    extend: {},
+  },
+  plugins: [],
+};
